608be99e3d103d107fa8f5cbb4dfaacb1c0b487c
Dick Karlsson <karlsson.dick39@gmail.com>
2018-03-13 13:03:46 +0100

    JQuery refact

--- code.js
+++ code.js
@@ -1,32 +1,56 @@
+// function renderMovie(movie){
+//     document.querySelector(".info #movietitle").textContent = movie.title;
+//     document.querySelector(".info #description").textContent = movie.description;
+//     document.querySelector(".poster").setAttribute("src", movie.imgUrl);
+
+//     let actorList = "";
+//     for(let i=0; i<movie.actors.length; i++){
+//         actorList += "<li>" + movie.actors[i] + "</li>";
+//     }
+//     document.querySelector(".info #actors").innerHTML = actorList;
+// }
+
 function renderMovie(movie){
-    document.querySelector(".info #movietitle").textContent = movie.title;
-    document.querySelector(".info #description").textContent = movie.description;
-    document.querySelector(".poster").setAttribute("src", movie.imgUrl);
+    $(".info #movietitle").text(movie.title);
+    $(".info #description").text(movie.description);
+    $(".poster").attr("src", movie.imgUrl);
 
-    let actorList = "";
+    $(".info #actors").empty();
     for(let i=0; i<movie.actors.length; i++){
-        actorList += "<li>" + movie.actors[i] + "</li>";
-    }
-    document.querySelector(".info #actors").innerHTML = actorList;
-}
-
-function changeStarRating(rating){
-    for(let i=1; i<=5; i++){
-        let star = document.getElementById("star" + i);
-        if (i <= rating){
-            star.classList.add("filled");
-        } else {
-            star.classList.remove("filled");
-        }
+        $(".info #actors").append("<li>" + movie.actors[i] + "</li>");
     }
 }
 
-for(let i=1; i<=5; i++){
-    let star = document.getElementById("star" + i);
-    star.addEventListener("click", function(){
-        changeStarRating(i);
-    });
+// function changeStarRating(rating){
+//     for(let i=1; i<=5; i++){
+//         let star = document.getElementById("star" + i);
+//         if (i <= rating){
+//             star.classList.add("filled");
+//         } else {
+//             star.classList.remove("filled");
+//         }
+//     }
+// }
+
+function changeStarRating(rating) {
+    $(".filled").removeClass("filled");
+
+    for(let i=1; i<=rating; i++){
+        $("[id=" + i + "]").addClass("filled");
+    }
 }
+
+// for(let i=1; i<=5; i++){
+//     let star = document.getElementById("star" + i);
+//     star.addEventListener("click", function(){
+//         changeStarRating(i);
+//     });
+// }
+
+$(".stars").on("click", "span", function(e){
+    let star = $(e.target);
+    let rating = parseInt(star.attr("id"));
+    changeStarRating(rating);
+});
 
 renderMovie(movieData);
-changeStarRating();
