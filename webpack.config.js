e60820363f6110121d17f1a35c994f1dd1eec5e4
Dick Karlsson <karlsson.dick39@gmail.com>
2018-03-14 09:58:53 +0100

    bundle, css och index i .gitignore

--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ module.exports = {
     mode: 'development', // whether we're building for dev or prod
     entry: './code.js', // which file to begin with
     output: {
-      path: path.resolve__dirname, // what folder to put bundle in
+      path: path.resolve(__dirname, "distribution"), // what folder to put bundle in
       filename: 'bundle.js' // what name to use for bundle
     },
     devServer: {
-        contentBase: path.join__dirname,// the root for the server
+        contentBase: path.join(__dirname, "distribution"),// the root for the server
         watchContentBase: true, // so we reload if other stuff like CSS changes
         port: 9000, // it'll now be at http://localhost:9000
     },
